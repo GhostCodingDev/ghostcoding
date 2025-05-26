f0723238658cf62a6bca96301c0c9c9831947793
Ghost Coding Development <dev@ghostcoding.tech>
2025-05-26 20:49:18 +0200

    First commit

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "main-ghost-coding.b-cdn.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  }
+};
+
+export default nextConfig;
